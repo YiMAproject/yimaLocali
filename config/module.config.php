105ba0e12e814d63f5ec4cbe1d29499c33dcd0d3
Payam Naderi <naderi.payam@gmail.com>
2014-04-06 18:54:56 +0430

    move default translation off to the yimaLocalize

--- config/module.config.php
+++ config/module.config.php
@@ -43,17 +43,4 @@ return array (
 		# if you need detected locale before continue of running application
 		'throw_exceptions' => false,
 	),
-
-    // Translator settings
-	'translator' => array(
-		'translation_file_patterns' => array(
-			array(
-				'type'     => 'phparray',
-				'base_dir' => __DIR__ . '/../language',
-				'pattern'  => '%s.php',
-                //yimaLocali translations using as default Text Domain
-                #'text_domain' => 'default',
-            ),
-		),
-	),
 );
