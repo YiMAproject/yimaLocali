22bb3a0c49048b33acac58ef959a2065409defc1
Payam Naderi <naderi.payam@gmail.com>
2014-05-04 16:08:57 +0430

    Using Plugin Manager Inside Locale(view/controller)Helper, we have to made locale plugins available through this pluginManager.

--- src/yimaLocali/Service/AbstractLocaleHelper.php
+++ src/yimaLocali/Service/AbstractLocaleHelper.php
@@ -1,6 +1,7 @@
 <?php
 namespace yimaLocali\Service;
 
+use Zend\ServiceManager\AbstractPluginManager;
 use Zend\ServiceManager\ServiceLocatorAwareInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
@@ -13,15 +14,20 @@ class AbstractLocaleHelper implements
     ServiceLocatorAwareInterface
 {
     /**
+     * @var string Locale
+     */
+    protected $locale;
+
+    /**
      * @var \Zend\Mvc\Controller\PluginManager|ServiceLocatorInterface
      */
     protected $serviceLocator;
 
     /**
-     * @var string Locale
+     * @var AbstractPluginManager
      */
-    protected $locale;
-	
+    protected $pluginManager;
+
     /**
      * Invoke as a functor
      *
@@ -101,6 +107,115 @@ class AbstractLocaleHelper implements
         }
 
         return false;
+    }
+
+    /**
+     * Set plugin manager instance
+     *
+     * @param  string|AbstractPluginManager $pluginManager
+     *
+     * @return $this
+     * @throws \Exception
+     */
+    public function setPluginManager($pluginManager)
+    {
+        if (is_string($pluginManager)) {
+            if (!class_exists($pluginManager)) {
+                throw new \Exception(
+                    sprintf(
+                        'Invalid helper helpers class provided (%s)',
+                        $pluginManager
+                    )
+                );
+            }
+
+            $pluginManager = new $pluginManager();
+        }
+
+        if (!$pluginManager instanceof AbstractPluginManager) {
+            throw new \Exception(
+                sprintf(
+                    'Helper helpers must extend Zend\ServiceManager\AbstractPluginManager; got type "%s" instead',
+                    (is_object($pluginManager) ? get_class($pluginManager) : gettype($pluginManager))
+                )
+            );
+        }
+
+        // inject locale to all plugin instance
+        $pluginManager->addInitializer(array($this, 'injectLocale'));
+
+        $this->pluginManager = $pluginManager;
+
+        return $this;
+    }
+
+    /**
+     * Inject a locale with the registered plugin instance
+     *
+     * @param  $helper
+     *
+     * @return void
+     */
+    public function injectLocale($helper)
+    {
+        if (! is_object($helper) || !method_exists($helper, 'setLocale')) {
+            // we're okay
+            return;
+        }
+
+        $locale = $this->getLocale();
+        $helper->setLocale($locale);
+    }
+
+    /**
+     * Get plugin manager instance
+     *
+     * @return AbstractPluginManager
+     */
+    public function getPluginManager()
+    {
+        if (null === $this->pluginManager) {
+            $this->setPluginManager(new PluginManager());
+        }
+
+        return $this->pluginManager;
+    }
+
+    /**
+     * Overloading: proxy to plugin helpers
+     *
+     * Proxies to the attached plugin manager to retrieve, return, and potentially
+     * execute helpers.
+     *
+     * * If the helper does not define __invoke, it will be returned
+     * * If the helper does define __invoke, it will be called as a functor
+     *
+     * @param  string $method
+     * @param  array $argv
+     *
+     * @return mixed
+     */
+    public function __call($method, $argv)
+    {
+        $helper = $this->plugin($method);
+        if (is_callable($helper)) {
+            return call_user_func_array($helper, $argv);
+        }
+
+        return $helper;
+    }
+
+    /**
+     * Get plugin instance
+     *
+     * @param  string     $name Name of plugin to return
+     * @param  null|array $options Options to pass to plugin constructor (if not already instantiated)
+     *
+     * @return object
+     */
+    public function plugin($name, array $options = null)
+    {
+        return $this->getPluginManager()->get($name, $options);
     }
 
     /**
---
+++ src/yimaLocali/Service/PluginManager.php
@@ -0,0 +1,39 @@
+<?php
+namespace yimaLocali\Service;
+
+use Zend\ServiceManager\AbstractPluginManager;
+use Zend\ServiceManager\ConfigInterface;
+
+class PluginManager extends AbstractPluginManager
+{
+	/**
+	 * Default set of helpers
+	 *
+	 * @var array
+	 */
+	protected $invokableClasses = array(
+//		'date' => 'cLocali\Plugin\Date',
+	);
+	
+	/**
+	 * Constructor
+	 *
+	 * After invoking parent constructor, add an initializer to inject the
+	 * attached renderer and translator, if any, to the currently requested helper.
+	 *
+	 * @param  null|ConfigInterface $configuration
+	 */
+	public function __construct(ConfigInterface $configuration = null)
+	{
+		parent::__construct($configuration);
+	}
+	
+    /**
+     * Validate the plugin
+     * 
+     */
+    public function validatePlugin($plugin)
+    {
+        return true;
+    }
+}
