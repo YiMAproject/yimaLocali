98f30b2f647ddeabff1b2c11e1ecd8662050ff2e
Payam Naderi <naderi.payam@gmail.com>
2015-01-04 14:28:53 +0330

    implement isRtl as plugin

--- config/module.config.php
+++ config/module.config.php
@@ -1,11 +1,11 @@
 <?php
-return array (
-	'yimaLocali' => array(
+return [
+	'yimaLocali' => [
         # Locale Detector Strategies, Implemented DetectorInterface
-		'detector' => array(
+		'detector' => [
             #  Used By Default AggregateStrategyFactory (yimaLocali.Detector.Strategy)
-            'aggregate' => array(
-                'strategies' => array(
+            'aggregate' => [
+                'strategies' => [
                     //'Registered\Service\Or\ClassName' => -10,
                     # or
                     /*
@@ -18,51 +18,63 @@ return array (
                     // default ordered strategies
                     'yimaLocali\Detector\UriPathStrategy' => 90,
                     'yimaLocali\Detector\CookieStrategy'  => 80,
-                    array(
+                    [
                         'object'   => new \yimaLocali\Detector\RestrictLocaleStrategy(),
                         'priority' => -1000
-                    ),
-                ),
-            ),
-        ),
+                    ],
+                ],
+            ],
+        ],
 
         # content of this key will pass to LocaleSupport(class) on Bootstrap
-        'available_locales' => array(
+        'available_locales' => [
             'default'    => 'en_US',
-            'locales'  => array(
-                'en_US', // locale with no options, will use system default
-                'nl_BE', // locale with no options, will use system default
-                'ar_AE' => array(
-                    'calendar'          => 'islamic',
-                ),
-                'fa_IR' => array(
+            'locales'  => [
+                'en_US' => [
                     # these are locale options default data, used or set by other modules
                     'default_time_zone' => 'Asia/Tehran',
                     'calendar'          => 'persian',
-                ),
-            ),
-            'aliases'    => array(
+
+                    'rtl'               => true, // used by isRtl() plugin
+                ], // locale with no options, will use system default
+                'nl_BE', // locale with no options, will use system default
+                'ar_AE' => [
+                    'calendar'          => 'islamic',
+                ],
+                'fa_IR' => [
+                    # these are locale options default data, used or set by other modules
+                    'default_time_zone' => 'Asia/Tehran',
+                    'calendar'          => 'persian',
+
+                    'rtl'               => true, // used by isRtl() plugin
+                ],
+            ],
+            'aliases'    => [
                 'en'    => 'en_US',
                 'ar'    => 'ar_AE',
                 'nl'    => 'nl_BE',
                 'fa'    => 'fa_IR',
                 'farsi' => 'fa',
-            ),
-        ),
+            ],
+        ],
 
 		# if you need detected locale before continue of running application
 		'throw_exceptions' => false,
-	),
+	],
 
     # Plugins services for locale() plugin manager
     # you can catch this plugins within view and controller:
     #  $this->locale()->test() or $this->plugin('test', $options);
-    'yima_locali_plugins' => array(
-//        'invokables' => array(),
-        /*'factories'  => array(
-            'test' => function ($sm) {
-                    return 'This is test';
+    'yima_locali_plugins' => [
+        #'invokables' => array(),
+        'factories'  => [
+            'isRtl' => function ($sl) {
+                $sm = $sl->getServiceLocator();
+                $locale = $sm->get('locale.detected');
+                $lData  = \yimaLocali\Service\LocaleRegistry::getLocaleData($locale);
+
+                return (!empty($lData) && isset($lData['rtl']) && $lData['rtl']);
             }
-        ),*/
-    ),
-);
+        ],
+    ],
+];
